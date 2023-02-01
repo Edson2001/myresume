44707102d07c2c6ab72518659503b61baed816e3
edson2001 <djiedson413@gmai.com>
2023-02-01 17:06:27 +0100

    feat: add env varible

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,29 @@
 import express from "express"
 import puppeteer from "puppeteer-core"
+import dotenv from "dotenv"
 const app = express()
+
+dotenv.config()
 
 app.get("/", async (req, res)=>{
 
-    const puppeteerCore = await puppeteer.launch({
-        executablePath: "C:\Program Files\Google\Chrome\Application\chrome.exe",
+    const browser = await puppeteer.launch({
+        executablePath: process.env.ENV_EXECUTABLEPATH || '',
         headless: false,
-        args:[
-            '--disable-gpu'
-        ]
     })
 
-    puppeteerCore.close()
+    const page = await browser.newPage()
+    await page.goto('https://google.com')
+    
+    await page.screenshot({
+        path: './app.png'
+    })
+
+    await page.pdf({
+        path: 'app.pdf'
+    })
+    
+    browser.close()
 
     return res.send("dlsd")
 
