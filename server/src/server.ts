f987f0aa9a7140fb58c72d3f799f5f226975c937
edson2001 <djiedson413@gmail.com>
2023-02-06 10:05:30 +0100

    fix: remove lib html-to-pdf

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,7 @@
 import express from "express"
 const app = express()
-import * as html_to_pdf from  'html-pdf-node'
-//import Convert from "./core/Convert"
 
 app.get("/", async (req, res)=>{
-    let options = { format: 'A4' };
-   console.log('l,,l,f,d,fd,fdl,fdl,fdl,')
-    let file = { url: "https://example.com" };
-    html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
-      console.log("PDF Buffer:-", pdfBuffer);
-    });
     return res.send("Server")
 })
 
